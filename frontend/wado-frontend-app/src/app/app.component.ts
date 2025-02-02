c0e5c74a580c9998c4c2cbd5de2c31558e111abe
Mihai Tomescu <79212170+Tomihai15@users.noreply.github.com>
2025-02-02 19:50:28 +0200

    css part2

--- frontend/wado-frontend-app/src/app/app.component.ts
+++ frontend/wado-frontend-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -14,7 +14,33 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   `,// Renders the active route
   standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wado-frontend-app';
+
+  ngOnInit(): void {
+    if (this.safeDocumentAccess()) {
+
+      const burger = document.querySelector('.navbar-burger');
+      const menu = document.querySelector('.navbar-menu');
+
+      if (burger && menu) {
+        burger.addEventListener('click', () => {
+          burger.classList.toggle('is-active');
+          menu.classList.toggle('is-active');
+        });
+      }
+    }
+  }
+
+  safeDocumentAccess() {
+    if (typeof document !== 'undefined') {
+      // Your code that accesses the document object
+      return true;
+    }
+    return false;
+  }
+
 }
+
+
 
