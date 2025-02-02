761764cd7825725dab77989df67211e5064034be
AndreeaJinga <andreea.jinga@yahoo.com>
2025-02-02 12:19:01 +0200

    added routes

--- frontend/wado-frontend-app/src/app/app.routes.ts
+++ frontend/wado-frontend-app/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { RegisterComponent } from './components/register/register.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { LoginComponent } from './components/login/login.component';
 import { ConceptPageComponent } from './components/pages/concept-page/concept-page.component';
+import { FrameworkInfoComponent } from './components/pages/framework-info/framework-info.component';
+import { InstancesComponent } from './components/pages/instances/instances.component';
+import { SparqlQueryComponent } from './components/pages/sparql-query/sparql-query.component';
+import { OneConceptPageComponent } from './components/pages/one-concept-page/one-concept-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' }, // Default page
@@ -10,4 +14,8 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'concept-page', component: ConceptPageComponent },
+  { path: 'framework-info', component: FrameworkInfoComponent },
+  { path: 'instances', component: InstancesComponent },
+  { path: 'sparql-query', component: SparqlQueryComponent },
+  { path: 'one-concept-page', component: OneConceptPageComponent },
 ];
